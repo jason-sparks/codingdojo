bebc060d40b763e350a66de3b682ac897c0c6293
Jason Sparks <hijasonsparks@gmail.com>
2020-08-13 16:43:53 -0700

    Added React Prop It Up assignment

---
+++ MERN/React/Prop It Up/prop-it-up/src/components/PersonCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class PersonCard extends Component {
+    render() {
+        const { firstName, lastName, age, hairColor } = this.props
+        return (
+            <>
+        <h2>{ lastName }, { firstName }</h2>
+        <p>Age: { age }</p>
+        <p>Hair Color: { hairColor }</p>
+            </>
+        );
+    }
+}
+
+export default PersonCard;
