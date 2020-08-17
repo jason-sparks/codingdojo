c90910b79ad9ae57c38794e8a1e0f6ea2892e1c7
Jason Sparks <hijasonsparks@gmail.com>
2020-08-16 21:06:18 -0700

    Removed import logo from React Prop It Up assignment and blank line from React First React With JSX

--- MERN/React/Prop It Up/prop-it-up/src/App.js
+++ MERN/React/Prop It Up/prop-it-up/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import PersonCard from "./components/PersonCard"
 
 function App() {
-  
   return (
     <div className="App">
       <PersonCard lastName="Doe" firstName="Jane" age="45" hairColor="Black"/>
