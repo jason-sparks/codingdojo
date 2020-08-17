90ade4b6b9e906e0f71d46cde31c88dadceef92b
Jason Sparks <hijasonsparks@gmail.com>
2020-08-17 01:25:39 -0700

    Added formatting to React Big Inversion assignment PersonCard component

--- MERN/React/Big Inversion/big-inversion/src/components/PersonCard.js
+++ MERN/React/Big Inversion/big-inversion/src/components/PersonCard.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
 const PersonCard = props => {
-    return (    <>
+    return (    
+    <>
         <h2>{ props.lastName }, { props.firstName }</h2>
         <p>Age: { props.age }</p>
         <p>Hair Color: { props.hairColor }</p>
-            </>
+    </>
             );
 }
 
