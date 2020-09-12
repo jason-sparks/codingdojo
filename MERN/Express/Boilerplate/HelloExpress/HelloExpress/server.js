26beb4b665d74e7d66075b3a5364aa9f510d4617
Jason Sparks <hijasonsparks@gmail.com>
2020-09-11 17:23:38 -0700

    Modified Express Boilerplate to have more verbose comments

--- MERN/Express/Boilerplate/HelloExpress/HelloExpress/server.js
+++ MERN/Express/Boilerplate/HelloExpress/HelloExpress/server.js
@@ -1,5 +1,15 @@
+// npm init -y HelloExpress // Creates package.json 
+// npm install express/ npm i express // installs express to the node_modules folder and package.json dependencies 
+
 const express = require("express");
 const app = express();
+
+// Start the express application server and listen on port 8000
+const server = app.listen(8000, () =>
+  console.log(`Server is locked and loaded on port ${server.address().port}!`)
+);
+
+// Routing in Express below
 
 // req is short for request
 // res is short for response
@@ -7,6 +17,4 @@ app.get("/api", (req, res) => {
   res.send("Our express api server is now sending this over to the browser");
 });
 
-const server = app.listen(8000, () =>
-  console.log(`Server is locked and loaded on port ${server.address().port}!`)
-);
+
